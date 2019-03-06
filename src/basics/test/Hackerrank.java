9f4f6aa56b68f4b437c789037a0cbd2f90f39a1b
Prasanna Rajagopalan <prasanna.rajagopalan@sap.com>
2019-03-06 22:20:08 +0530

    New

--- src/basics/test/Hackerrank.java
+++ src/basics/test/Hackerrank.java
@@ -41,6 +41,8 @@ public class Hackerrank {
     Java’s Arrays.sort method uses Quicksort for arrays of primitives and merge sort for arrays of objects.
     Arrays.sort(arr); applicable for list using Collections.sort(al);
     Arrays.sort(arr, Collections.reverseOrder());
+    Arrays.binarySearch(array, key)
+    Collections.binarySearch(array, key)
 
     int[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
 
@@ -50,15 +52,29 @@ public class Hackerrank {
      Arrays.sort(arr, 1, 5);
      */
 
-    // Insertion sort is where every element is picked up and sorted one by one.
     /*
+    Insertion sort
+    Insertion sort is where every element is picked up and sorted one by one.
+
+    How to find time complexity?
+    In the best case, where the array was already sorted, no element will need to be moved, so the algorithm will
+    just run through the array once and return the sorted array. The running time would be directly proportional to
+    the size of the input, so we can say it will take N time.
+
+    However, we usually focus on the worst-case running time (computer scientists are pretty pessimistic). The worst
+    case for Insertion Sort occurs when the array is in reverse order. To insert each number, the algorithm will have
+    to shift over that number to the beginning of the array. Sorting the entire array of N numbers will therefore take
+    1+2+..N-1 operations, which is N(N-1)/2 (almost N^2). Computer scientists just round that up
+    (pick the dominant term)
+    to N^2 and say that Insertion Sort is an " time" algorithm.
+
     1 4 3 5 6 2
     1 3 4 5 6 2
     1 3 4 5 6 2
     1 3 4 5 6 2
     1 2 3 4 5 6
     */
-    static void insertionSort2(int n, int[] arr) {
+    static void insertionSort(int n, int[] arr) {
         int temp;
         for(int i=0;i<n;i++) {
             for(int j=i; j>0;j--) {
@@ -75,5 +91,77 @@ public class Hackerrank {
                 System.out.println("");
             }
         }
+    }
+
+    /*
+    Bubble Sort
+    Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent
+    elements if they are in wrong order.
+
+    Ends with n-i-1 because last element is continuously sorted and pushed to last in every iteration
+    Bubble sort has worst-case and average complexity both О(n2)
+    Performance of bubble sort over an already-sorted list (best-case) is O(n).
+     */
+
+    static void bubbleSort(int n, int[] arr) {
+        int temp;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j]>arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    /*
+    Selection sort
+    The selection sort algorithm sorts an array by repeatedly finding the minimum element
+    (considering ascending order) from unsorted part and putting it at the beginning.
+
+    Selection sort has worst-case and average complexity both О(n2)
+    Performance of bubble sort over an already-sorted list (best-case) is O(n).
+     */
+
+    static void selectionSort(int n, int[] arr) {
+        int temp;
+        for(int i=0; i<n; i++) {
+            int min = i;
+            for(int j=i+1; j<n; j++) {
+                if(arr[j]<arr[min]) {
+                    min=j;
+                }
+            }
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    /*
+    Binary Search
+
+    List needs to be sorted for this to work. If not, results will be undefined
+
+    This method runs in log(n) time for a “random access” list like ArrayList.
+    If the specified list does not implement the RandomAccess interface and is large, this method
+    will do an iterator-based binary search that performs O(n) link traversals and O(log n) element comparisons.
+     */
+
+    static int binarySearch(int[] arr, int start, int end, int key) {
+        if(end>=1) {
+            int mid = start + (end+start)/2;
+
+            // Found case
+            if(arr[mid]==key)
+                return mid;
+            if(arr[mid]>key)
+                return binarySearch(arr,start,mid,key);
+            return binarySearch(arr, mid+1, end, key);
+        }
+        // Not found
+        return -1;
     }
 }
